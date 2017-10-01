436610473604abfb03c15fb5167d3274e0b81c31
David Waterman <davidwaterman@Davids-MacBook-Air.local>
2017-10-01 18:55:04 -0500

    test

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 console.log("Working at " + PORT);
 
-app.use(express.static('app'));
+app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
