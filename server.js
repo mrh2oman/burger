4a7f339839c15772768c1fe123fdd64464c5bcc8
David Waterman <davidwaterman@Davids-MacBook-Air.local>
2017-09-30 19:53:01 -0500

    Final

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.set('view engine', 'handlebars');
 var router = require('./controllers/burgers_controller.js');
 app.use('/', router);
 
-var port = process.env.PORT || 3000;
-app.listen(port);
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0');
