8a29633c716782b2e2a9999cdc1c6c8197b2c872
David Waterman <davidwaterman@Davids-MacBook-Air.local>
2017-09-30 21:01:39 -0500

    im done

--- orm/models/todo.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(sequelize, Datatypes) {
-    var Todo = sequelize.define("Todo", {
-        test: DataTypes.STRING,
-        complete: DataTypes.BOOLEAN
-    });
-    return Todo
-};
